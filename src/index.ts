18e7e010265fe884f58c8426f59f9aa196cdc3bc
SoraKumo <info@croud.jp>
2020-04-30 14:36:57 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+class Sync<T> {
+  private promise: Promise<T>;
+  private resolve!: (value: T) => void;
+  constructor() {
+    this.promise = new Promise((resolve) => {
+      this.resolve = resolve;
+    });
+  }
+  exec(proc: () => Promise<T>) {
+    return async () => {
+      const result = await proc();
+      this.resolve(result);
+      return result;
+    };
+  }
+  lock() {
+    return this.promise;
+  }
+}
+export const beforeAllAsync = <T>(proc: () => Promise<T>, timeout?: number) => {
+  const sync = new Sync<T>();
+  beforeAll(sync.exec(proc), timeout);
+  return sync.lock();
+};
+export const afterAllAsync = <T>(proc: () => Promise<T>, timeout?: number) => {
+  const sync = new Sync<T>();
+  afterAll(sync.exec(proc), timeout);
+  return sync.lock();
+};
+export const testAsync = <T>(
+  name: string,
+  proc: () => Promise<T>,
+  timeout?: number
+): Promise<T> => {
+  const sync = new Sync<T>();
+  it.concurrent(name, sync.exec(proc), timeout);
+  return sync.lock();
+};
---
+++ test/index.test.ts
@@ -0,0 +1,63 @@
+import { testAsync, beforeAllAsync } from "jest-parallel";
+
+const sleep = (value: number) =>
+  new Promise((resolve) => setTimeout(resolve, value));
+
+describe("Test", () => {
+  const initWait = beforeAllAsync(async () => {
+    await sleep(1000);
+    console.log("beforeAll");
+  });
+  const A = testAsync("A", async () => {
+    await sleep(1000);
+    await initWait;
+    console.log("A");
+    return 100;
+  });
+  const B = testAsync("B", async () => {
+    await sleep(5000);
+    await initWait;
+    console.log("B");
+    return 200;
+  });
+  testAsync("C", async () => {
+    await sleep(3000);
+    const value = (await A) + (await B);
+    console.log("C", value);
+    expect(value).toBe(300);
+  });
+  testAsync("D", async () => {
+    await sleep(2000);
+    console.log("D");
+  });
+});
+
+/*
+ 1,000 + 1,000 + 5,000 + 3,000 + 2,000 = 12,000ms
+               ↓ Parallel execution
+            5,705ms
+
+ PASS  src/index.test.ts (5.705s)
+  ● Console
+    console.log
+      beforeAll
+      at src/index.test.ts:9:13
+    console.log
+      A
+      at src/index.test.ts:14:13
+    console.log
+      D
+      at src/index.test.ts:31:13
+    console.log
+      B
+      at src/index.test.ts:20:13
+    console.log
+      C 300
+      at src/index.test.ts:26:13
+
+Test Suites: 1 passed, 1 total
+Tests:       4 passed, 4 total
+Snapshots:   0 total
+Time:        6.288s
+Ran all test suites.
+*/
